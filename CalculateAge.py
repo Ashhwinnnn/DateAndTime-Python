966896e84012318db3c313a2e5bac85d9fefd163
R. Ashwin <Ashwin3104@gmail.com>
2025-06-25 11:57:38 +0530

    Add files via upload

---
+++ CalculateAge.py
@@ -0,0 +1,13 @@
+from datetime import date
+
+def age(dob):
+    today = date.today()
+    years = today.year - dob.year
+
+    if (today.month,today.day)<(dob.month,dob.day):
+        years-=1
+
+        return years
+    
+print('age:', age(date(2000,7,1)))
+
---
+++ StringToDate.py
@@ -0,0 +1,8 @@
+import datetime
+
+str_date=input('Enter Date in DD-MM-YYYY format:')
+d,m,y=str_date.split('-')
+
+d1=datetime.date(int(y),int(m),int(d))
+
+print('date:',d1)
